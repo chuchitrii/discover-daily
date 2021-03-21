c3a1a93d943a8d6692c10e166b55d01c4baaa8f3
chuchitrii <vza2k@yandex.ru>
2021-03-21 22:21:36 +0300

    - filters descriptions

--- src/app/components/search-filters/search-filters.component.ts
+++ src/app/components/search-filters/search-filters.component.ts
@@ -8,6 +8,39 @@ import { FormGroup } from '@angular/forms';
 })
 export class SearchFiltersComponent implements OnInit {
   form: FormGroup;
+  inputs: { name: string; min?: number; max?: number; step?: number }[] = [
+    {
+      name: 'acousticness',
+    },
+    {
+      name: 'danceability',
+    },
+    {
+      name: 'energy',
+    },
+    {
+      name: 'instrumentalness',
+    },
+    {
+      name: 'liveness',
+      // not sure
+    },
+    {
+      name: 'popularity',
+      min: 0,
+      max: 100,
+      step: 1,
+    },
+    {
+      name: 'tempo',
+      min: 0,
+      max: 300,
+      step: 1,
+    },
+    {
+      name: 'valence',
+    },
+  ];
 
   constructor() {}
 
